e9013811acc6e7807bdc4ac4712f23d20fa6f9ef
Tamplier <araman777@gmail.com>
2025-10-27 15:55:08 +0500

    Update pickle base test

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 import pytest
 import spacy
 import numpy as np
+from sentence_transformers import SentenceTransformer
 from src.transformers import SpacyTokenizer, SbertVectorizer
 from src.util import CachingSpellChecker, typos_processor, PathHelper
 
@@ -47,15 +48,29 @@ def test_path_helper():
     assert 'models/' in str(PathHelper.models.label_encoder.resolve())
     assert 'data/processed/' in str(PathHelper.data.processed.x_train.resolve())
 
-
-def test_pickle_base():
+def test_pickle_base(monkeypatch):
+    counting_init(SentenceTransformer, monkeypatch)
+    SbertVectorizer._bo_storage.clear()
     tokenizer = SpacyTokenizer()
     vectorizer = SbertVectorizer()
     assert tokenizer.nlp is not None
     assert vectorizer.model is not None
     assert vectorizer.tokenizer is not None
+    assert SentenceTransformer.created == 1
+
+    vectorizer2 = SbertVectorizer()
+    vectorizer2.model
+    assert SentenceTransformer.created == 1
 
     t_objects = list(tokenizer._bo_storage.keys())
     v_objects = list(vectorizer._bo_storage.keys())
     np.testing.assert_array_equal(t_objects, ['nlp'])
     np.testing.assert_array_equal(v_objects, ['model', 'tokenizer'])
+
+def counting_init(c, monkeypatch):
+    c.created = 0
+    original_init = c.__init__
+    def custom_init(self, *args, **kwargs):
+        type(self).created += 1
+        original_init(self, *args, **kwargs)
+    monkeypatch.setattr(c, "__init__", custom_init)
